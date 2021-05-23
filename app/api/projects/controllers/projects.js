aa796660782435608acc29c1bbfee6a535d67c27
Patryk Jezierski <patryk.jezierski@gis-support.pl>
2021-05-23 15:06:12 +0200

    overwritten default controllers

--- app/api/projects/controllers/projects.js
+++ app/api/projects/controllers/projects.js
@@ -6,4 +6,20 @@ const { sanitizeEntity } = require("strapi-utils");
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    const entities = await strapi.services.projects.find(ctx.query);
+
+    return entities.map((entity) => {
+      const project = sanitizeEntity(entity, { model: strapi.models.projects });
+      delete project.description;
+      return project;
+    });
+  },
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.projects.findOne({ name: id });
+    return sanitizeEntity(entity, { model: strapi.models.projects });
+  },
+};
